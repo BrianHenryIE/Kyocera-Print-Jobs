70f207a87079ea528961c8f7622964d70e3d5d4d
Brian Henry <brian.henry2@gmail.com>
2015-11-11 14:00:25 +0000

    Logging in working

--- src/test/java/ie/brianhenry/kyocerajobs/ServiceTest.java
+++ src/test/java/ie/brianhenry/kyocerajobs/ServiceTest.java
@@ -79,8 +79,6 @@ public class ServiceTest {
 
 		HttpPost httpPost = new HttpPost("https://" + printerIp + "/startwlm/login.cgi");
 
-		
-		
 		// Set headers
 
 		httpPost.setHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
@@ -100,34 +98,140 @@ public class ServiceTest {
 				"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/46.0.2490.80 Safari/537.36");
 
 		// Set form data
+		// It didn't work without the empty ones
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
 		// params.add(new BasicNameValuePair("arg01_UserName", username));
 		// params.add(new BasicNameValuePair("arg02_Password", password));
 
-		params.add(new BasicNameValuePair("failhtmfile", "/startwlm/Start_Wlm.htm"));
+//		params.add(new BasicNameValuePair("failhtmfile", "/startwlm/Start_Wlm.htm"));
 		params.add(new BasicNameValuePair("okhtmfile", "/startwlm/Start_Wlm.htm"));
 		params.add(new BasicNameValuePair("func", "authLogin"));
 		params.add(new BasicNameValuePair("arg03_LoginType", "_mode_off"));
 		params.add(new BasicNameValuePair("arg04_LoginFrom", "_wlm_login"));
-		params.add(new BasicNameValuePair("language", "../wlmeng/index.htm"));
-		params.add(new BasicNameValuePair("hiddRefreshDevice", "../startwlm/Hme_DvcSts.htm"));
-		params.add(new BasicNameValuePair("hiddRefreshPanelUsed", "../startwlm/Hme_PnlUsg.htm"));
-		params.add(new BasicNameValuePair("hiddRefreshPaperid", "../startwlm/Hme_Paper.htm"));
-		params.add(new BasicNameValuePair("hiddRefreshTonerid", "../startwlm/Hme_StplPnch.htm"));
-		params.add(new BasicNameValuePair("hiddRefreshStapleid", "../startwlm/Hme_Toner.htm"));
-		params.add(new BasicNameValuePair("hiddnBackNavIndx", "1"));
+//		params.add(new BasicNameValuePair("language", "../wlmeng/index.htm"));
+//		params.add(new BasicNameValuePair("hiddRefreshDevice", "../startwlm/Hme_DvcSts.htm"));
+//		params.add(new BasicNameValuePair("hiddRefreshPanelUsed", "../startwlm/Hme_PnlUsg.htm"));
+//		params.add(new BasicNameValuePair("hiddRefreshPaperid", "../startwlm/Hme_Paper.htm"));
+//		params.add(new BasicNameValuePair("hiddRefreshTonerid", "../startwlm/Hme_StplPnch.htm"));
+//		params.add(new BasicNameValuePair("hiddRefreshStapleid", "../startwlm/Hme_Toner.htm"));
+//		params.add(new BasicNameValuePair("hiddnBackNavIndx", "1"));
 
-		params.add(new BasicNameValuePair("hiddRefreshDevice", "../startwlm/Hme_DvcSts.htm"));
-		params.add(new BasicNameValuePair("hiddRefreshPanelUsed", "../startwlm/Hme_PnlUsg.htm"));
-		params.add(new BasicNameValuePair("hiddRefreshPaperid", "../startwlm/Hme_Paper.htm"));
-		params.add(new BasicNameValuePair("hiddRefreshTonerid", "../startwlm/Hme_StplPnch.htm"));
-		params.add(new BasicNameValuePair("hiddRefreshStapleid", "../startwlm/Hme_Toner.htm"));
-		params.add(new BasicNameValuePair("hiddnBackNavIndx", "0"));
+//		params.add(new BasicNameValuePair("hiddRefreshDeviceBack", ""));
+//		params.add(new BasicNameValuePair("hiddRefreshPanelUsedBack", ""));
+//		params.add(new BasicNameValuePair("hiddRefreshPaperidBack", ""));
+//		params.add(new BasicNameValuePair("hiddRefreshToneridBack", ""));
+//		params.add(new BasicNameValuePair("hiddRefreshStapleidBack", ""));
+//		params.add(new BasicNameValuePair("hiddCompatibility", ""));
+//		params.add(new BasicNameValuePair("hiddPasswordToOpenChk", ""));
+//		params.add(new BasicNameValuePair("hiddPasswordToOpen", ""));
+//		params.add(new BasicNameValuePair("hiddRePasswordToOpen", ""));
+//		params.add(new BasicNameValuePair("hiddPasswordToEditChk", ""));
+//		params.add(new BasicNameValuePair("hiddPasswordToEdit", ""));
+//		params.add(new BasicNameValuePair("hiddRePasswordToEdit", ""));
+//		params.add(new BasicNameValuePair("hiddPrinting", ""));
+//		params.add(new BasicNameValuePair("hiddChanges", ""));
+//		params.add(new BasicNameValuePair("hiddCopyingOfText", ""));
+//		params.add(new BasicNameValuePair("hiddEmaiSID", ""));
+//		params.add(new BasicNameValuePair("hiddEmaiName", ""));
+//		params.add(new BasicNameValuePair("hiddECM", ""));
+//		params.add(new BasicNameValuePair("hiddDocID", ""));
+//		params.add(new BasicNameValuePair("privid", ""));
+//		params.add(new BasicNameValuePair("publicid", ""));
+//		params.add(new BasicNameValuePair("attrtype", ""));
+//		params.add(new BasicNameValuePair("attrname", ""));
+//		params.add(new BasicNameValuePair("hiddFolderType", ""));
+//		params.add(new BasicNameValuePair("hiddFolderSMBType", ""));
+//		params.add(new BasicNameValuePair("hiddFolderFTPType", ""));
+//		params.add(new BasicNameValuePair("hiddSMBNumber1", ""));
+//		params.add(new BasicNameValuePair("hiddSMBNumber2", ""));
+//		params.add(new BasicNameValuePair("hiddSMBNumber3", ""));
+//		params.add(new BasicNameValuePair("hiddSMBNumber4", ""));
+//		params.add(new BasicNameValuePair("hiddSMBNumber5", ""));
+//		params.add(new BasicNameValuePair("hiddSMBNumber6", ""));
+//		params.add(new BasicNameValuePair("hiddSMBNumber7", ""));
+//		params.add(new BasicNameValuePair("hiddFTPNumber1", ""));
+//		params.add(new BasicNameValuePair("hiddFTPNumber2", ""));
+//		params.add(new BasicNameValuePair("hiddFTPNumber3", ""));
+//		params.add(new BasicNameValuePair("hiddFTPNumber4", ""));
+//		params.add(new BasicNameValuePair("hiddFTPNumber5", ""));
+//		params.add(new BasicNameValuePair("hiddFTPNumber6", ""));
+//		params.add(new BasicNameValuePair("hiddFTPNumber7", ""));
+//		params.add(new BasicNameValuePair("hiddFAXAddress1", ""));
+//		params.add(new BasicNameValuePair("hiddFAXAddress2", ""));
+//		params.add(new BasicNameValuePair("hiddFAXAddress3", ""));
+//		params.add(new BasicNameValuePair("hiddFAXAddress4", ""));
+//		params.add(new BasicNameValuePair("hiddFAXAddress5", ""));
+//		params.add(new BasicNameValuePair("hiddFAXAddress6", ""));
+//		params.add(new BasicNameValuePair("hiddFAXAddress7", ""));
+//		params.add(new BasicNameValuePair("hiddFAXAddress8", ""));
+//		params.add(new BasicNameValuePair("hiddFAXAddress9", ""));
+//		params.add(new BasicNameValuePair("hiddFAXAddress10", ""));
+//		params.add(new BasicNameValuePair("hiddIFaxAdd", ""));
+//		params.add(new BasicNameValuePair("hiddIFaxViaServer", ""));
+//		params.add(new BasicNameValuePair("hiddIFaxConnMode", ""));
+//		params.add(new BasicNameValuePair("hiddIFaxResolution", ""));
+//		params.add(new BasicNameValuePair("hiddIFaxResolution1", ""));
+//		params.add(new BasicNameValuePair("hiddIFaxResolution2", ""));
+//		params.add(new BasicNameValuePair("hiddIFaxResolution3", ""));
+//		params.add(new BasicNameValuePair("hiddIFaxResolution4", ""));
+//		params.add(new BasicNameValuePair("hiddIFaxResolution5", ""));
+//		params.add(new BasicNameValuePair("hiddIFaxComplession", ""));
+//		params.add(new BasicNameValuePair("hiddIFaxPaperSize", ""));
+//		params.add(new BasicNameValuePair("hiddIFaxPaperSize1", ""));
+//		params.add(new BasicNameValuePair("hiddIFaxPaperSize2", ""));
+//		params.add(new BasicNameValuePair("hiddIFaxPaperSize3", ""));
+//		params.add(new BasicNameValuePair("hiddImage", ""));
+//		params.add(new BasicNameValuePair("hiddTest", ""));
+//		params.add(new BasicNameValuePair("hiddDoc_Num", ""));
+//		params.add(new BasicNameValuePair("hiddCopy", ""));
+//		params.add(new BasicNameValuePair("hiddDocument", ""));
+//		params.add(new BasicNameValuePair("hiddDocRec", ""));
+//		params.add(new BasicNameValuePair("AddressNumberPersonal", ""));
+//		params.add(new BasicNameValuePair("AddressNumberGroup", ""));
+//		params.add(new BasicNameValuePair("hiddPersonAddressID", ""));
+//		params.add(new BasicNameValuePair("hiddGroupAddressID", ""));
+//		params.add(new BasicNameValuePair("hiddPrnBasic", ""));
+//		params.add(new BasicNameValuePair("hiddPageName", ""));
+//		params.add(new BasicNameValuePair("hiddDwnLoadType", ""));
+//		params.add(new BasicNameValuePair("hiddPrintType", ""));
+//		params.add(new BasicNameValuePair("hiddSend1", ""));
+//		params.add(new BasicNameValuePair("hiddSend2", ""));
+//		params.add(new BasicNameValuePair("hiddSend3", ""));
+//		params.add(new BasicNameValuePair("hiddSend4", ""));
+//		params.add(new BasicNameValuePair("hiddSend5", ""));
+//		params.add(new BasicNameValuePair("hiddAddrBokBackUrl", ""));
+//		params.add(new BasicNameValuePair("hiddAddrBokNumber", ""));
+//		params.add(new BasicNameValuePair("hiddAddrBokName", ""));
+//		params.add(new BasicNameValuePair("hiddAddrBokFname", ""));
+//		params.add(new BasicNameValuePair("hiddSendFileName", ""));
+//		params.add(new BasicNameValuePair("hiddenAddressbook", ""));
+//		params.add(new BasicNameValuePair("hiddenAddressbook1", ""));
+//		params.add(new BasicNameValuePair("hiddSendDoc_Num", ""));
+//		params.add(new BasicNameValuePair("hiddSendColor", ""));
+//		params.add(new BasicNameValuePair("hiddSendAddInfo", ""));
+//		params.add(new BasicNameValuePair("hiddSendFileFormat", ""));
+//		params.add(new BasicNameValuePair("hiddMoveConfScn", ""));
+//
+//		params.add(new BasicNameValuePair("hiddRefreshDevice", "../startwlm/Hme_DvcSts.htm"));
+//		params.add(new BasicNameValuePair("hiddRefreshPanelUsed", "../startwlm/Hme_PnlUsg.htm"));
+//		params.add(new BasicNameValuePair("hiddRefreshPaperid", "../startwlm/Hme_Paper.htm"));
+//		params.add(new BasicNameValuePair("hiddRefreshTonerid", "../startwlm/Hme_StplPnch.htm"));
+//		params.add(new BasicNameValuePair("hiddRefreshStapleid", "../startwlm/Hme_Toner.htm"));
+//		params.add(new BasicNameValuePair("hiddnBackNavIndx", "0"));
+//
+//		params.add(new BasicNameValuePair("hiddRefreshDeviceBack", ""));
+//		params.add(new BasicNameValuePair("hiddRefreshPanelUsedBack", ""));
+//		params.add(new BasicNameValuePair("hiddRefreshPaperidBack", ""));
+//		params.add(new BasicNameValuePair("hiddRefreshToneridBack", ""));
+//		params.add(new BasicNameValuePair("hiddRefreshStapleidBack", ""));
+//		params.add(new BasicNameValuePair("hiddValue", ""));
+		params.add(new BasicNameValuePair("arg05_AccountId", ""));
+		params.add(new BasicNameValuePair("arg06_DomainName", ""));
 
 		params.add(new BasicNameValuePair("arg01_UserName", "Admin"));
 		params.add(new BasicNameValuePair("arg02_Password", "Admin"));
 		params.add(new BasicNameValuePair("Login", "Login"));
-		params.add(new BasicNameValuePair("hndHeight", "0"));
+//		params.add(new BasicNameValuePair("hndHeight", "0"));
 
 		httpPost.setEntity(new UrlEncodedFormEntity(params));
 
@@ -141,7 +245,19 @@ public class ServiceTest {
 		System.out.println(response.getStatusLine().getReasonPhrase());
 		System.out.println(response.toString());
 
-		assertEquals(response.getStatusLine().getStatusCode(), 200);
+		assertEquals(200, response.getStatusLine().getStatusCode());
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		client.close();
 
 	}
